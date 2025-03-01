c68409e3b4ad4d29faa4337a10c9446f901d7165
Ithairony <ithairony21@gmail.com>
2025-03-01 18:27:02 +0000

    Adding Test methods

--- src/griffith/ConversionsTest.java
+++ src/griffith/ConversionsTest.java
@@ -3,6 +3,29 @@
 
 package griffith;
 
-public class ConversionsTest {
+import static org.junit.jupiter.api.Assertions.fail;
 
+import org.junit.jupiter.api.Test;
+
+public class ConversionsTest {
+	@Test
+	void testEuroToDollar( ) {	
+		fail("Not yet implemented");
+	}
+	@Test
+	void testDollaToEuro() {
+		fail("Not yet implemented");		
+	}	
+	@Test
+	void testStringToInteger () {
+		fail("Not yet implemented");
+	}
+	@Test
+	void testIntegerToString() {
+		fail("Not yet implemented");
+	}
+	@Test
+	void testSwitchCase() {
+		fail("Not yet implemented");
+	}
 }
