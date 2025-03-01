476fd96a886d5937d1083d851f21cec464980053
Ithairony <ithairony21@gmail.com>
2025-03-01 20:30:35 +0000

    testDollarToEuro test for negatives

--- src/griffith/ConversionsTest.java
+++ src/griffith/ConversionsTest.java
@@ -22,9 +22,9 @@ public class ConversionsTest {
 	@Test
 	void testDollarToEuro() {
 //		expected /	actual  / delta ( range of acceptance / message 					
-	assertEquals(1, Conversions.dollarToEuro(0.96), 0.02, "0.96$ should grant you 1€");	// Test for Positive values
+	//assertEquals(1, Conversions.dollarToEuro(0.96), 0.02, "0.96$ should grant you 1€");	// Test for Positive values
 	assertEquals(-0.96, Conversions.dollarToEuro(-1), 0.02, "-0.96$ should grant you -1€");	// Test for Negative values
-    assertEquals(0, Conversions.dollarToEuro(0), 0.01, "0$ should grant you €");	// Test for 0
+    //assertEquals(0, Conversions.dollarToEuro(0), 0.01, "0$ should grant you €");	// Test for 0
 	}	
 	/*
 	@Test
