22f88c0cf2ee981d3ee8b7f7a9b3596ccef86415
Ithairony <ithairony21@gmail.com>
2025-03-01 20:28:50 +0000

    testDollarToEuro test for positive

--- src/griffith/ConversionsTest.java
+++ src/griffith/ConversionsTest.java
@@ -18,11 +18,15 @@ public class ConversionsTest {
 		assertEquals(-0.96, Conversions.euroToDollar(-1), 0.02, "-1€ should grant you -0.96$");	// Test for Negative values
         assertEquals(0, Conversions.euroToDollar(0), 0.01, "0€ should be 0$");	// Test for 0
 	}
-	/*
+	
 	@Test
 	void testDollarToEuro() {
-		fail("Not yet implemented");		
+//		expected /	actual  / delta ( range of acceptance / message 					
+	assertEquals(1, Conversions.dollarToEuro(0.96), 0.02, "0.96$ should grant you 1€");	// Test for Positive values
+	assertEquals(-0.96, Conversions.dollarToEuro(-1), 0.02, "-0.96$ should grant you -1€");	// Test for Negative values
+    assertEquals(0, Conversions.dollarToEuro(0), 0.01, "0$ should grant you €");	// Test for 0
 	}	
+	/*
 	@Test
 	void testStringToInteger () {
 		fail("Not yet implemented");
